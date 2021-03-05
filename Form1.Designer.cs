e5514b17d396ec117055740c56497dc4c430e1bf
Cirno <43587824+BelovedTomboyishGirl@users.noreply.github.com>
2021-03-05 16:58:38 +0100

    Initial Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Nioh_2_Save_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadOriginalSave = new System.Windows.Forms.Button();
+            this.LoadNewSave = new System.Windows.Forms.Button();
+            this.ConvertSavegameButton = new System.Windows.Forms.Button();
+            this.originalSaveFileName = new System.Windows.Forms.Label();
+            this.newSaveFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoadOriginalSave
+            // 
+            this.LoadOriginalSave.Location = new System.Drawing.Point(12, 12);
+            this.LoadOriginalSave.Name = "LoadOriginalSave";
+            this.LoadOriginalSave.Size = new System.Drawing.Size(162, 23);
+            this.LoadOriginalSave.TabIndex = 0;
+            this.LoadOriginalSave.Text = "Load Original Save File";
+            this.LoadOriginalSave.UseVisualStyleBackColor = true;
+            this.LoadOriginalSave.Click += new System.EventHandler(this.LoadOriginalSave_Click);
+            // 
+            // LoadNewSave
+            // 
+            this.LoadNewSave.Location = new System.Drawing.Point(12, 43);
+            this.LoadNewSave.Name = "LoadNewSave";
+            this.LoadNewSave.Size = new System.Drawing.Size(162, 24);
+            this.LoadNewSave.TabIndex = 1;
+            this.LoadNewSave.Text = "Load New Save File";
+            this.LoadNewSave.UseVisualStyleBackColor = true;
+            this.LoadNewSave.Click += new System.EventHandler(this.LoadNewSave_Click);
+            // 
+            // ConvertSavegameButton
+            // 
+            this.ConvertSavegameButton.Location = new System.Drawing.Point(55, 73);
+            this.ConvertSavegameButton.Name = "ConvertSavegameButton";
+            this.ConvertSavegameButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertSavegameButton.TabIndex = 3;
+            this.ConvertSavegameButton.Text = "Convert";
+            this.ConvertSavegameButton.UseVisualStyleBackColor = true;
+            this.ConvertSavegameButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // originalSaveFileName
+            // 
+            this.originalSaveFileName.AutoSize = true;
+            this.originalSaveFileName.Location = new System.Drawing.Point(180, 17);
+            this.originalSaveFileName.Name = "originalSaveFileName";
+            this.originalSaveFileName.Size = new System.Drawing.Size(0, 13);
+            this.originalSaveFileName.TabIndex = 4;
+            // 
+            // newSaveFileName
+            // 
+            this.newSaveFileName.AutoSize = true;
+            this.newSaveFileName.Location = new System.Drawing.Point(180, 49);
+            this.newSaveFileName.Name = "newSaveFileName";
+            this.newSaveFileName.Size = new System.Drawing.Size(0, 13);
+            this.newSaveFileName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(9, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "by Github.com/BelovedTomboyishGirl";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 154);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newSaveFileName);
+            this.Controls.Add(this.originalSaveFileName);
+            this.Controls.Add(this.ConvertSavegameButton);
+            this.Controls.Add(this.LoadNewSave);
+            this.Controls.Add(this.LoadOriginalSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadOriginalSave;
+        private System.Windows.Forms.Button LoadNewSave;
+        private System.Windows.Forms.Button ConvertSavegameButton;
+        private System.Windows.Forms.Label originalSaveFileName;
+        private System.Windows.Forms.Label newSaveFileName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Nioh_2_Save_Converter
+{
+    public partial class Form1 : Form
+    {
+        Byte[] loadedOldSaveGame;
+        Byte[] loadedNewSaveGame;
+        string saveFolder;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Informations: \nYou should have created a new savegame (and be sure to backed up the old one!!!) \n The program refer Original Save as the savegame you want to convert, and New Save as the new file it'll be converted from, you imperatively need to create a new save (and BACKUP ORIGINAL SAVE) before using this program.\nConverted Save will erase newly created savegame. \n You should do that for SAVEDATAXX (XX=Slot Number) and for SYSTEMSAVEDATAXX.");
+        }
+
+        private void LoadOriginalSave_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                string DocumentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.InitialDirectory = DocumentsFolder + "\\KoeiTecmo\\NIOH2\\SaveData";
+                openFileDialog.Filter = "SAVEDATA.BIN (*.BIN)|*.bin";
+                openFileDialog.FilterIndex = 2;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    loadedOldSaveGame = File.ReadAllBytes(filePath);
+                    //Checking if it a valid safe file by comparing first and second byte
+                    if (loadedOldSaveGame[0] != 0x7F && loadedOldSaveGame[1] != 0x9C)
+                    {
+                        MessageBox.Show("Error, this isn't a valid save file!");
+                        Array.Clear(loadedOldSaveGame, 0, loadedOldSaveGame.Length);
+                    }
+                    else
+                    {
+                        originalSaveFileName.Text = openFileDialog.FileName;
+                    }
+                }
+            }
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            if (loadedOldSaveGame?.Length > 0 && loadedNewSaveGame?.Length > 0)
+            {
+                DialogResult confirmDialog = MessageBox.Show($"The save will convert \n {originalSaveFileName.Text} (Your true save game)\n " +
+                    $"to\n{newSaveFileName.Text} (New save)\nAre you sure you want to proceed ?", "Confirm ?", MessageBoxButtons.YesNoCancel);
+                if (confirmDialog == DialogResult.Yes)
+                {
+                    Array.Copy(loadedNewSaveGame, 0x10, loadedOldSaveGame, 0x10, 4);
+                    try
+                    {
+                        File.WriteAllBytes(saveFolder, loadedOldSaveGame);
+                        MessageBox.Show("Complete!");
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show($"{exception.Message}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, one of the file have not been loaded !");
+            }
+        }
+
+        private void LoadNewSave_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                string DocumentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.InitialDirectory = DocumentsFolder + "\\KoeiTecmo\\NIOH2\\SaveData";
+                openFileDialog.Filter = "SAVEDATA.BIN (*.BIN)|*.bin";
+                openFileDialog.FilterIndex = 2;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    loadedNewSaveGame = File.ReadAllBytes(filePath);
+                    //Checking if it a valid safe file by comparing first and second byte
+                    if (loadedNewSaveGame[0] != 0x7F && loadedNewSaveGame[1] != 0x9C) 
+                    {
+                        MessageBox.Show("Error, this isn't a valid save file!");
+                        Array.Clear(loadedNewSaveGame, 0, loadedNewSaveGame.Length);
+                    }
+                    else
+                    {
+                        newSaveFileName.Text = openFileDialog.FileName;
+                    }
+                    saveFolder = filePath;
+                }
+            }
+        }
+
+    }
+}
